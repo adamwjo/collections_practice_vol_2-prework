71cd434c231a4a18e59c36aeacfd041e60900899
Adam Johnson <adamwjo@gmail.com>
2018-08-23 12:51:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,8 +3,9 @@ def begins_with_r(array)
   array.each do |item|
     if item[0] != "r"
       return false
-    elsif item[0] == "r"
-      return true
+    else
+      if item[0] == "r"
+        return true
     end
   end
 end
